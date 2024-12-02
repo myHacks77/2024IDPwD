b27da381f9d9310723522a86d59cc9445aeaa217
goldenFingerMY <seahan0709@icloud.com>
2024-12-02 17:07:28 +0800

    feat: style the glass container

--- src/app/pages/welcome/welcome.component.ts
+++ src/app/pages/welcome/welcome.component.ts
@@ -1,11 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-welcome',
-  imports: [],
   templateUrl: './welcome.component.html',
-  styleUrl: './welcome.component.scss'
+  styleUrls: ['./welcome.component.scss']
 })
-export class WelcomeComponent {
+export class WelcomeComponent implements OnInit {
+  private multiple = 25;
+  private element: HTMLElement | undefined;
 
+  constructor(private el: ElementRef) {}
+
+  ngOnInit() {
+    this.element = this.el.nativeElement.querySelector('.welcome-container');
+    document.addEventListener('mousemove', (e) => {
+      window.requestAnimationFrame(() => {
+        this.transformElement(e.clientX, e.clientY);
+      });
+    });
+    document.addEventListener('mouseleave', () => {
+      if (this.element) {
+        this.element.style.transform = "rotateX(0) rotateY(0)";
+      }
+    });
+  }
+  private transformElement(x: number, y: number) {
+    if (!this.element) return;
+    const box = this.element.getBoundingClientRect();
+    const calcX = -(y - box.y - (box.height / 2)) / this.multiple;
+    const calcY = (x - box.x - (box.width / 2)) / this.multiple;
+    
+    this.element.style.transform = `rotateX(${calcX}deg) rotateY(${calcY}deg)`;
+  }
 }
