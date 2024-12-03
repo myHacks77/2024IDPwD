3cecedc00072e11fe6897474f047ae62e16a1910
goldenFingerMY <seahan0709@icloud.com>
2024-12-03 09:35:19 +0800

    refactor: move glass card listeners to app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { Component, ElementRef, Inject, PLATFORM_ID, Renderer2 } from '@angular/core';
+import { CommonModule, isPlatformBrowser } from '@angular/common';
 import { NavigationEnd, Router, RouterModule } from '@angular/router';
 import { NavigationComponent } from './navigation/navigation.component';
 
@@ -16,13 +16,37 @@ import { NavigationComponent } from './navigation/navigation.component';
 })
 export class AppComponent {
   isAnimating = false;
+  name: string | undefined;
+  private multiple = 25;
+  private element: HTMLElement | undefined;
 
-  constructor(private router: Router) {
+  constructor(
+    private el: ElementRef,
+    private renderer: Renderer2,
+    @Inject(PLATFORM_ID) private platformId: Object,
+    private router: Router
+  ) {
     this.router.events.subscribe(event => {
       if (event instanceof NavigationEnd) {
         this.startAnimation();
       }
-    });
+    })
+
+    if (isPlatformBrowser(this.platformId)) {
+      this.element = this.el.nativeElement.querySelector('.glass-container');
+      
+      this.renderer.listen('document', 'mousemove', (e: MouseEvent) => {
+        window.requestAnimationFrame(() => {
+          this.transformElement(e.clientX, e.clientY);
+        });
+      });
+
+      this.renderer.listen('document', 'mouseleave', () => {
+        if (this.element) {
+          this.renderer.setStyle(this.element, 'transform', 'rotateX(0) rotateY(0)');
+        }
+      });
+    }
   }
 
   startAnimation() {
@@ -30,5 +54,25 @@ export class AppComponent {
     setTimeout(() => {
       this.isAnimating = false;
     }, 2000);
+  }
+
+  private transformElement(x: number, y: number) {
+    if (!this.element) return;
+    
+    const box = this.element.getBoundingClientRect();
+    let calcX = -(y - box.y - (box.height / 2)) / this.multiple;
+    let calcY = (x - box.x - (box.width / 2)) / this.multiple;
+   
+    calcX = calcX > 10 ? 10 : calcX;
+    calcY = calcY > 10 ? 10 : calcY;
+
+    calcX = calcX < -10 ? -10 : calcX;
+    calcY = calcY < -10 ? -10 : calcY;
+
+    this.renderer.setStyle(
+      this.element, 
+      'transform', 
+      `rotateX(${calcX}deg) rotateY(${calcY}deg)`
+    );
   }
 }
--- src/app/pages/welcome/welcome.component.ts
+++ src/app/pages/welcome/welcome.component.ts
@@ -1,5 +1,4 @@
-import { Component, OnInit, ElementRef, Renderer2, PLATFORM_ID, Inject } from '@angular/core';
-import { isPlatformBrowser } from '@angular/common';
+import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { FormsModule } from '@angular/forms';
 
@@ -12,57 +11,14 @@ import { FormsModule } from '@angular/forms';
   ],
   standalone: true
 })
-export class WelcomeComponent implements OnInit {
+export class WelcomeComponent {
   name: string | undefined;
-  private multiple = 25;
-  private element: HTMLElement | undefined;
 
   constructor(
-    private el: ElementRef,
-    private renderer: Renderer2,
-    @Inject(PLATFORM_ID) private platformId: Object,
     private router: Router
-  ) {}
-
-  ngOnInit() {
-    if (isPlatformBrowser(this.platformId)) {
-      this.element = this.el.nativeElement.querySelector('.glass-container');
-      
-      this.renderer.listen('document', 'mousemove', (e: MouseEvent) => {
-        window.requestAnimationFrame(() => {
-          this.transformElement(e.clientX, e.clientY);
-        });
-      });
-
-      this.renderer.listen('document', 'mouseleave', () => {
-        if (this.element) {
-          this.renderer.setStyle(this.element, 'transform', 'rotateX(0) rotateY(0)');
-        }
-      });
-    }
-  }
+  ) { }
 
   enterGame() {
     this.router.navigate(['/hello-friend']);
-  }
-
-  private transformElement(x: number, y: number) {
-    if (!this.element) return;
-    
-    const box = this.element.getBoundingClientRect();
-    let calcX = -(y - box.y - (box.height / 2)) / this.multiple;
-    let calcY = (x - box.x - (box.width / 2)) / this.multiple;
-   
-    calcX = calcX > 10 ? 10 : calcX;
-    calcY = calcY > 10 ? 10 : calcY;
-
-    calcX = calcX < -10 ? -10 : calcX;
-    calcY = calcY < -10 ? -10 : calcY;
-
-    this.renderer.setStyle(
-      this.element, 
-      'transform', 
-      `rotateX(${calcX}deg) rotateY(${calcY}deg)`
-    );
   }
 }
