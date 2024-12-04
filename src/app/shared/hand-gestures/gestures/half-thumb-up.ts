8c4b6168a169c8259b4ace0c45b345d0355a75de
goldenFingerMY <seahan0709@icloud.com>
2024-12-04 15:38:04 +0800

    fix: fix half thumb recognize issue

--- src/app/shared/hand-gestures/gestures/half-thumb-up.ts
+++ src/app/shared/hand-gestures/gestures/half-thumb-up.ts
@@ -3,7 +3,9 @@ import { PredefinedHandposes } from '../handpose.types';
 
 export const HALF_THUMB_UP_GESTURE = new GestureDescription(PredefinedHandposes.HalfThumbUp);
 
-HALF_THUMB_UP_GESTURE.addCurl(Finger.Thumb, FingerCurl.HalfCurl, 1);HALF_THUMB_UP_GESTURE.addCurl(Finger.Thumb, FingerCurl.FullCurl, .8);
+HALF_THUMB_UP_GESTURE.addCurl(Finger.Thumb, FingerCurl.NoCurl, .7);
+HALF_THUMB_UP_GESTURE.addCurl(Finger.Thumb, FingerCurl.HalfCurl, 1);
+HALF_THUMB_UP_GESTURE.addCurl(Finger.Thumb, FingerCurl.FullCurl, 1);
 HALF_THUMB_UP_GESTURE.addDirection(Finger.Thumb, FingerDirection.VerticalUp, 1.0);
 
 HALF_THUMB_UP_GESTURE.addCurl(Finger.Index, FingerCurl.FullCurl, 1.0);
