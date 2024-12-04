8b475e9b771e83649bf9c9555473bdd075b24c9e
goldenFingerMY <seahan0709@icloud.com>
2024-12-04 09:09:30 +0800

    feat: add draft v2 sign language game

--- src/app/shared/sign-language-game/sign-language-game.component.ts
+++ src/app/shared/sign-language-game/sign-language-game.component.ts
@@ -1,14 +1,69 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, AfterViewInit, Input, OnChanges, SimpleChanges } from '@angular/core';
 import { HandGesturesComponent } from '../hand-gestures/hand-gestures.component';
+import { CommonModule } from '@angular/common';
+import { PredefinedHandposes } from '../hand-gestures/handpose.types';
+import { throttleTime } from 'rxjs/operators';
+import { Router } from '@angular/router';
+
+
+export interface GameSetting {
+  levelTitle: string;
+  nextStepLink: string;
+  requiredGestures: GameWord[];
+}
+
+export interface GameWord {
+  word: string;
+  gestures: PredefinedHandposes[];
+  isCorrect?: boolean;
+}
 
 @Component({
   selector: 'app-sign-language-game',
+  standalone: true,
   imports: [
+    CommonModule,
     HandGesturesComponent
   ],
   templateUrl: './sign-language-game.component.html',
   styleUrl: './sign-language-game.component.scss'
 })
-export class SignLanguageGameComponent {
+export class SignLanguageGameComponent implements AfterViewInit, OnChanges {
+  @ViewChild(HandGesturesComponent) handGesturesComponent!: HandGesturesComponent;
+  @Input() gameSetting!: GameSetting;
+  progress: number = 0;
+  isWon: boolean = false;
+  // TODO: timer
+
+  constructor(private router: Router) {}
+
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log(changes);
+  }
+
+  ngAfterViewInit(): void {
+    this.handGesturesComponent?.handpose$?.pipe(
+      throttleTime(1000)
+    ).subscribe((hands) => {
+
+      if(!this.gameSetting.requiredGestures.length)return;
+      if(this.isWon)return;
+      
+      const targetGesture = this.gameSetting.requiredGestures[this.progress];
+      if (hands.some(hand => targetGesture.gestures.includes(hand.handpose))) {
+        this.progress++;
+        console.log(this.progress);
+      }
+
+      if (this.progress === this.gameSetting.requiredGestures.length) {
+        this.isWon = true;
+      }
+    });
+  }
+
+  nextLevel(): void {
+    if (!this.gameSetting.nextStepLink) return;
+    this.router.navigate([this.gameSetting.nextStepLink]);
+  }
 
 }
