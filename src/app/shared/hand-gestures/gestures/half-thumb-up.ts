19fddd10e3381a3c92546a967bd51365c71388f3
goldenFingerMY <seahan0709@icloud.com>
2024-12-04 15:42:45 +0800

    fix: fix half thumb estimator

--- src/app/shared/hand-gestures/gestures/half-thumb-up.ts
+++ src/app/shared/hand-gestures/gestures/half-thumb-up.ts
@@ -3,7 +3,7 @@ import { PredefinedHandposes } from '../handpose.types';
 
 export const HALF_THUMB_UP_GESTURE = new GestureDescription(PredefinedHandposes.HalfThumbUp);
 
-HALF_THUMB_UP_GESTURE.addCurl(Finger.Thumb, FingerCurl.NoCurl, .7);
+HALF_THUMB_UP_GESTURE.addCurl(Finger.Thumb, FingerCurl.NoCurl, .8);
 HALF_THUMB_UP_GESTURE.addCurl(Finger.Thumb, FingerCurl.HalfCurl, 1);
 HALF_THUMB_UP_GESTURE.addCurl(Finger.Thumb, FingerCurl.FullCurl, 1);
 HALF_THUMB_UP_GESTURE.addDirection(Finger.Thumb, FingerDirection.VerticalUp, 1.0);
