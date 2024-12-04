5d56abc4e0aafead4d9307228f418fdd9fa8b9af
goldenFingerMY <seahan0709@icloud.com>
2024-12-04 19:06:57 +0800

    fix: enhance "point" gesture

--- src/app/shared/hand-gestures/gestures/point-to-other.ts
+++ src/app/shared/hand-gestures/gestures/point-to-other.ts
@@ -3,12 +3,9 @@ import { PredefinedHandposes } from '../handpose.types';
 
 export const POINT_TO_OTHER_GESTURE = new GestureDescription(PredefinedHandposes.PointToOther);
 
-for (let finger of [Finger.Thumb, Finger.Index, Finger.Middle, Finger.Ring, Finger.Pinky]) {
-  POINT_TO_OTHER_GESTURE.addCurl(finger, FingerCurl.NoCurl, 1.0);
-  POINT_TO_OTHER_GESTURE.addDirection(finger, FingerDirection.VerticalUp, 0.8);
-  POINT_TO_OTHER_GESTURE.addDirection(finger, FingerDirection.VerticalDown, 0.8);
-  POINT_TO_OTHER_GESTURE.addDirection(finger, FingerDirection.DiagonalUpRight, 0.4);
-  POINT_TO_OTHER_GESTURE.addDirection(finger, FingerDirection.DiagonalUpLeft, 0.4);
-  POINT_TO_OTHER_GESTURE.addDirection(finger, FingerDirection.DiagonalDownRight, 0.4);
-  POINT_TO_OTHER_GESTURE.addDirection(finger, FingerDirection.DiagonalDownLeft, 0.4);
+for (let finger of [Finger.Thumb,Finger.Middle,  Finger.Ring, Finger.Pinky]) {
+  POINT_TO_OTHER_GESTURE.addCurl(finger, FingerCurl.FullCurl, 1.0);
+  POINT_TO_OTHER_GESTURE.addCurl(finger, FingerCurl.HalfCurl, 1.0);
 }
+
+POINT_TO_OTHER_GESTURE.addCurl(Finger.Index, FingerCurl.NoCurl, 1.0);
