35b7dac384402aaf431898bc02411c1e17d7615b
goldenFingerMY <seahan0709@icloud.com>
2024-12-04 09:10:24 +0800

    feat: add come together setting

--- src/app/pages/come-together/come-together.component.ts
+++ src/app/pages/come-together/come-together.component.ts
@@ -1,16 +1,35 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { HandGesturesComponent } from '../../shared/hand-gestures/hand-gestures.component';
+import { PredefinedHandposes } from '../../shared/hand-gestures/handpose.types';
+import { GameSetting, SignLanguageGameComponent } from '../../shared/sign-language-game/sign-language-game.component';
 
 @Component({
   selector: 'app-come-together',
   imports: [
-    HandGesturesComponent
+    SignLanguageGameComponent,
+    CommonModule
   ],
   templateUrl: './come-together.component.html',
   styleUrl: './come-together.component.scss'
 })
 export class ComeTogetherComponent {
+
+  isEntered = false;
+  gameSetting: GameSetting = {
+    levelTitle: 'Come Together',
+    nextStepLink: '/leaderboard',
+    requiredGestures: [
+      { gestures: [PredefinedHandposes.PointToOther], word: 'Ni' },
+      { gestures: [PredefinedHandposes.TwoFingers], word: 'Chi' },
+      { gestures: [PredefinedHandposes.TwoFingers], word: 'Fan' },
+      { gestures: [PredefinedHandposes.PointDiagonalUp], word: 'le' },
+      { gestures: [PredefinedHandposes.PointDiagonalDown], word: 'ma' },
+      { gestures: [PredefinedHandposes.PointDiagonalUp], word: 'le' },
+      { gestures: [PredefinedHandposes.PointDiagonalDown], word: 'ma' },  
+    ]
+  };
+
   startGame() {
-    // this.handGestures.nativeElement.style.display = 'block';
+    this.isEntered = true;
   }
 }
