a385f35468aa6af81ff7d6fb09499ace333b17ae
goldenFingerMY <seahan0709@icloud.com>
2024-12-04 23:16:40 +0800

    feat: add random flowers field

--- src/app/pages/leaderboard/leaderboard.component.ts
+++ src/app/pages/leaderboard/leaderboard.component.ts
@@ -1,11 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FlowerComponent } from './flower/flower.component';
+import { CommonModule } from '@angular/common';
+
+interface FlowerPosition {
+  name: string;
+  x: number;
+  y: number;
+}
 
 @Component({
   selector: 'app-leaderboard',
-  imports: [],
+  standalone: true,
+  imports: [FlowerComponent, CommonModule],
   templateUrl: './leaderboard.component.html',
-  styleUrl: './leaderboard.component.scss'
+  styleUrls: ['./leaderboard.component.scss']
 })
-export class LeaderboardComponent {
-
+export class LeaderboardComponent implements OnInit {
+  flowers: FlowerPosition[] = [];
+  names = ['Amory', 'Jenny', 'John', 'Jane', 'Tom', 'Jerry', 'Amy', 'Lily', 'Lucy', 'Lily', 'Lucy', 'Lily', 'Lucy'];
+  
+  ngOnInit() {
+    // 为每个名字生成随机位置
+    this.flowers = this.names.map(name => ({
+      name,
+      x: Math.random() * 80, // 0-80%，留出边距
+      y: Math.random() * 80
+    }));
+  }
 }
